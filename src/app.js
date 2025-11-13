8bae6facc18917d266c9b3ba8150d5c5fc7c439f
TejeswarAchari <vteja797@gmail.com>
2025-11-13 10:27:31 +0530

    Creating Express Server and Handling Routes

--- src/app.js
+++ src/app.js
@@ -1,13 +1,20 @@
 const express = require("express")
 const app = express()
-app.use("/test",(req,res)=>{
-    res.send("Hello from all")
+
+app.get("/user",(req,res)=>{
+    res.send({"Name":"Tejeswar Achari","Age":20})
 })
-app.use((req,res)=>{
-    res.send("Hello Node")
+app.post("/user",(req,res)=>{
+    res.send("File Saved")
+
+})
+app.delete("/user",(req,res)=>{
+    res.send("File Deleted Sucessfully")
 })
 
+app.patch("/user",(req,res)=>{
+    res.send("Patched Sucessfully")
+})
 app.listen(3000,()=>{
-    console.log("Server Started at 3000...")
+    console.log("Server Started at PORT 3000...")
 })
-
